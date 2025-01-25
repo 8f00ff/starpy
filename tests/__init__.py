7c0f9ffc6f1c0705c06627ee05259679b3d57ad0
8f00ff (Vi) <8f00ff@proton.me>
2025-01-25 02:09:08 -0500

    project setup

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Tests: Unit tests for StarPy spaceship simulator.
+
+Author: 8f00ff (Vi)
+Version: 1.0.0
+License: MIT
+"""
---
+++ tests/modules/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Tests: Unit tests for ship modules.
+
+Author: 8f00ff (Vi)
+Version: 1.0.0
+License: MIT
+"""
